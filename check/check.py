ea5620ec59c1e4c33ac1e05cf3fc818130094dfa
EternalllZM <43221983+EternalllZM@users.noreply.github.com>
2024-07-31 12:50:06 -0500

    attempt to inject BML into check

--- check/check.py
+++ check/check.py
@@ -11,7 +11,7 @@ _ = Translator("Check", __file__)
 class Check(commands.Cog):
     """Check"""
 
-    __version__ = "2.1.0"
+    __version__ = "2.1.0-dev1"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         # Thanks Sinbad! And Trusty in whose cogs I found this.
@@ -27,6 +27,7 @@ class Check(commands.Cog):
         self.log = logging.getLogger("red.cog.dav-cogs.check")
 
     @commands.command()
+    @commands.guild_only()
     @checks.mod()
     @commands.max_concurrency(1, commands.BucketType.guild)
     async def check(self, ctx, member: discord.Member):
@@ -41,7 +42,71 @@ class Check(commands.Cog):
             await self._maybe_altmarker(ctx, member)
             await self._warnings_or_read(ctx, member)
             await self._maybe_listflag(ctx, member)
+    async def listcases(
+        self,
+        ctx: commands.Context,
+        per_embed: Optional[commands.Range[int, 1, 19]] = 6,
+        *,
+        member: Union[discord.Member, int],
+    ):
+        """List cases for the specified member."""
+        async with ctx.typing():
+            try:
+                if isinstance(member, int):
+                    cases = await get_cases_for_member(
+                        bot=ctx.bot, guild=ctx.guild, member_id=member
+                    )
+                else:
+                    cases = await get_cases_for_member(bot=ctx.bot, guild=ctx.guild, member=member)
+            except discord.NotFound:
+                return await ctx.send("That user does not exist.")
+            except discord.HTTPException:
+                return await ctx.send(
+                    "Something unexpected went wrong while fetching that user by ID."
+                )
+            if not cases:
+                return await ctx.send("That user does not have any cases.")
 
+            rendered_cases = []
+            for page, ccases in enumerate(chunks(cases, per_embed), 1):
+                embed = discord.Embed(
+                    title=f"Cases for `{getattr(member, 'display_name', member)}` (Page {page} / {len(cases) // per_embed + 1 if len(cases) % per_embed else len(cases) // per_embed})",
+                )
+                for case in ccases:
+                    if case.moderator is None:
+                        moderator = "Unknown"
+                    elif isinstance(case.moderator, int):
+                        if case.moderator == 0xDE1:
+                            moderator = "Deleted User."
+                        else:
+                            translated = "Unknown or Deleted User"
+                            moderator = f"[{translated}] ({case.moderator})"
+                    else:
+                        moderator = f"{case.moderator} ({case.moderator.id})"
+
+                    length = ""
+                    if case.until:
+                        start = datetime.fromtimestamp(case.created_at, tz=timezone.utc)
+                        end = datetime.fromtimestamp(case.until, tz=timezone.utc)
+                        end_fmt = discord.utils.format_dt(end)
+                        duration = end - start
+                        dur_fmt = cf.humanize_timedelta(timedelta=duration)
+                        until = f"Until: {end_fmt}\n"
+                        duration = f"Length: {dur_fmt}\n"
+                        length = until + duration
+
+                    created_at = datetime.fromtimestamp(case.created_at, tz=timezone.utc)
+                    embed.add_field(
+                        name=f"Case #{case.case_number} | {(await get_casetype(case.action_type, getattr(member, 'guild', ctx.guild))).case_str}",
+                        value=f"{cf.bold('Moderator:')} {moderator}\n"
+                        f"{cf.bold('Reason:')} {case.reason}\n"
+                        f"{length}"
+                        f"{cf.bold('Timestamp:')} {discord.utils.format_dt(created_at)}\n\n",
+                        inline=False,
+                    )
+                rendered_cases.append(embed)
+
+        await menu(ctx, rendered_cases)
         await ctx.send(_("Lookup completed."))
 
     async def _userinfo(self, ctx, member):
